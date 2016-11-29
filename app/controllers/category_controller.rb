b811761c1d96f79cf89cd64d487cac103b73a29e
CJordanGoodman <good.cjordan@gmail.com>
2016-11-29 14:04:39 -0800

    whoops

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,5 +1,5 @@
 class CategoryController < ActionController::Base
-    protect_from_forgery, :with => :exception
+    protect_from_forgery#, :with => :exception
     layout "base"
     before_action :authenticate_user!
 

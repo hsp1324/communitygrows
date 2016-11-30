a48e374452c0090ae6061fb608d13a13e1d1f05b
Elmer Diaz <elmerd@berkeley.edu>
2016-11-30 02:17:58 +0000

    finished updating templates

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,13 +3,13 @@ class NotificationMailer < ApplicationMailer
   def announcement_email(user,announcement)
   	@user = user
   	@announcement = announcement
-    mail(to: @user.email, subject: 'New Announcement Created: ' + @announcement.title)
+    mail(to: @user.email, subject: 'A New announcment from CG: ' + @announcement.title)
   end
 
   def announcement_update_email(user, announcement)
   	@user = user
   	@announcement = announcement
-    mail(to: @user.email, subject: 'Announcement Updated: ' + @announcement.title)
+    mail(to: @user.email, subject: 'A CG announcement has been updated: ' + @announcement.title)
   end
 
   def new_document_email(user,document)

6f285c6812d7acc2401a5667c115031d56eff5e0
Dongjoo Shin <sdjinn111@berkeley.edu>
2017-07-12 00:58:41 +0000

    d

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,6 @@ ActiveRecord::Schema.define(version: 20161130015803) do
     t.string   "description"
     t.datetime "date"
     t.string   "url"
-    t.datetime "end"
   end
 
   create_table "expertises", force: :cascade do |t|
@@ -106,33 +105,33 @@ ActiveRecord::Schema.define(version: 20161130015803) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                     default: "",                    null: false
-    t.string   "encrypted_password",                        default: "",                    null: false
+    t.string   "email",                  default: "",                    null: false
+    t.string   "encrypted_password",     default: "",                    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                             default: 0,                     null: false
+    t.integer  "sign_in_count",          default: 0,                     null: false
     t.datetime "current_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                                null: false
-    t.datetime "updated_at",                                                                null: false
+    t.datetime "created_at",                                             null: false
+    t.datetime "updated_at",                                             null: false
     t.boolean  "admin"
-    t.datetime "last_sign_in_at",                           default: '2016-11-02 16:12:50', null: false
+    t.datetime "last_sign_in_at",        default: '2017-07-01 21:37:07', null: false
     t.boolean  "internal"
     t.boolean  "external"
     t.boolean  "executive"
-    t.string   "digest_pref"
     t.string   "name"
     t.string   "board_role"
     t.string   "committee"
-    t.text     "about_me",               limit: 4294967295
-    t.text     "why_join",               limit: 4294967295
-    t.boolean  "tilted"
+    t.text     "about_me"
+    t.text     "why_join"
+    t.string   "digest_pref"
     t.text     "ethnicity"
     t.text     "gender"
     t.text     "current_company"
     t.text     "current_position"
+    t.boolean  "tilted"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

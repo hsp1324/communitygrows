dd36d623f0c33a44ce0af26c9689c89af5d78bbe
Noah Lopez <noah_lopez@noahs-macbook.local>
2017-07-21 18:44:56 -0700

    feat: added the ability to delete committee names in committee dashboard

--- app/controllers/committee_controller.rb
+++ app/controllers/committee_controller.rb
@@ -2,6 +2,7 @@ class CommitteeController < ApplicationController
     layout "base"
     
     def index
+        @committees = Commitee.all
     end
 
     def new_committee
@@ -33,6 +34,59 @@ class CommitteeController < ApplicationController
                 redirect_to committee_index_path
             end
         end
+    end
+
+    def delete_committee
+        if !current_user.admin
+            flash[:message] = "Only admins can create committees."
+            redirect_to root_path and return
+        end
+        @id = params[:id] 
+        @committee = Commitee.find(@id)
+        @committee.destroy!
+        flash[:notice] = "committee with name #{@committee.name} deleted successfully."
+        redirect_to committee_index_path
+    end       
+
+    def edit_committee
+        if !current_user.admin
+            flash[:message] = "Only admins can create committees."
+            redirect_to root_path and return
+        end
+        @id = params[:id] 
+        @committee = Commitee.find(@id)
+        @committees = Commitee.all
+    end
+
+    def update_committee
+        if !current_user.admin
+            flash[:message] = "Only admins can create committees."
+            redirect_to root_path and return
+        end
+        @committee = Commitee.find(params[:id])
+        committee = params[:committee]
+        if committee[:name].to_s == ''
+            flash[:notice] = "Please fill in the committee name field."
+            redirect_to edit_committee_path
+        elsif Commitee.has_name?(committee[:name].to_s)
+            flash[:notice] = "The committee name provided already exists. Please enter a different name."
+            redirect_to edit_committe_path
+        else
+            @committee = Commitee.find(params[:id])
+            committee = params[:committee]
+            if committee[:name].to_s == '' then
+                flash[:notice] = "Please fill in the committee name field."
+                redirect_to edit_committee_path
+            elsif Commitee.has_name?(committee[:name].to_s)
+                flash[:notice] = "The committee name provided already exists. Please enter a different name."
+                redirect_to edit_committee_path
+            else
+                @committee.update_attributes!(:name => committee[:name].to_s)
+                flash[:notice] = "Categroy with name [#{@committee.name}] updated successfully and email was successfully sent."
+                redirect_to committee_index_path
+            end
+        end
+
     end
     
 end

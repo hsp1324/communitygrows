b6769fe55f5de3cf60b1ed67c5a6d1fa5fe0815b
Wen Chien Yen <cyen7115@berkeley.edu>
2017-07-27 00:53:35 +0000

    added a few files but did not include paths.rb

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,3 +1,4 @@
+#/features/step_definitions/admin_steps.rb
 # By Tony. Steps needed for user testing
 Given /^a valid admin$/ do
   @user = User.create!({
@@ -8,9 +9,28 @@ Given /^a valid admin$/ do
              :admin => true
            })
 end
+# By Wen
+Given /^a valid admin ([a-zA-z]*)$/ do |name|
+  @user = User.create!({
+             :name => name,
+             :email => "admindummy@dummy.com",
+             :password => "admindummypass",
+             :password_confirmation => "admindummypass",
+             :admin => true
+           })
+end
 
 Given /^a logged in admin$/ do
   step "a valid admin"
+  visit "/users/sign_in"
+  fill_in "user_email", :with => "admindummy@dummy.com"
+  fill_in "password", :with => "admindummypass"
+  click_button "Log in"
+end
+
+# By Wen
+Given /^a logged in admin "([^"]*)"$/ do |name|
+  step "a valid admin #{name}"
   visit "/users/sign_in"
   fill_in "user_email", :with => "admindummy@dummy.com"
   fill_in "password", :with => "admindummypass"
--- features/step_definitions/committee_steps.rb
+++ features/step_definitions/committee_steps.rb
@@ -5,3 +5,30 @@ Given /the following committees exist/ do |committees_table|
         Committee.create!(committee)
     end
 end
+
+# By Wen. Assign users to given committee
+# sets up a member inside of a committee
+Given /^user "([^\"]*)" is in committee "([^\"]*)"$/ do |user_name, committee_name|
+    User.find_by(name: user_name).committees << Committee.find_by(name: committee_name)
+end
+
+Given /^admin (.*) is in committee (.*)$/ do |user_name, committee_name|
+    User.find_by(name: user_name, admin: true).committees << Committee.find_by(name: committee_name)
+end
+
+Given /^user "([^\"]*)" is in the following committees: "([^\"]*)"$/ do |user_name, committees|
+    committees.split(',').each do |committee_name|
+        committee_name = committee_name.strip
+        step "user #{user_name} is in committee #{committee_name}"
+    end
+end 
+
+Given /^admin "([^\"]*)" is in the following committees: "([^\"]*)"$/ do |admin_name, committees|
+    committees.split(',').each do |committee|
+        step "admin #{admin_name} is in committee #{committee.strip}"
+    end
+end 
+
+
+
+

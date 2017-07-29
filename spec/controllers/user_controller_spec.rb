10d986d6e49edfef62f35390164303489137d7cc
Dongjoo Shin <sdjinn111@berkeley.edu>
2017-07-29 02:00:30 +0000

    fix in User_controller_Spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -70,37 +70,4 @@ describe UserController do
     end
 end
 
-RSpec.describe "User announcements/calendar", :type => :request do
-  describe "CommunityGrows website" do
-    before 'should redirect to the dashboard when user is signed in' do
-      curr = User.create!(:name => "Rspec", :email => "admin@communitygrows.org", :password => "communitygrowsrocks", :password_confirmation => "communitygrowsrocks", :admin => true)
-      visit "/users/sign_in"
-      fill_in :user_email,    :with => curr.email
-      fill_in :user_password, :with => curr.password
-      click_button "Log in"
-      expect(page).to have_content("Dashboard")
 
-      
-      click_link "New Announcement"
-      expect(page).to have_content("Title")
-      expect(page).to have_content("Content")
-      fill_in "Title", :with => 'abcd'
-      fill_in "Content", :with => 'bcd'
-      click_button "Submit"
-      expect(page).to have_content("abcd")
-      expect(page).to have_content("bcd")
-      
-      click_link "Edit Announcement", match: :first
-      expect(page).to have_content("Title")
-      expect(page).to have_content("Content")
-      fill_in "Title", :with => 'ccc'
-      fill_in "Content", :with => 'ddddd'
-      click_button "Submit"
-      expect(page).to have_content("ccc")
-      expect(page).to have_content("ddddd")
-      click_link "Delete Announcement", match: :first
-      expect(page).to have_content("Announcement Management")
-    end
-    
-  end
-end

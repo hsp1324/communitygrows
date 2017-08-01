6f4999c81b699bbe32f2a5a667f939b2d166fd33
wjpark1 <willpark16@gmail.com>
2017-07-31 18:05:22 -0700

    Added version number to various migrations

--- db/migrate/20160426215256_add_committee_type_to_documents.rb
+++ db/migrate/20160426215256_add_committee_type_to_documents.rb
@@ -1,4 +1,4 @@
-class AddCommitteeTypeToDocuments < ActiveRecord::Migration
+class AddCommitteeTypeToDocuments < ActiveRecord::Migration[5.0]
   def change
     add_column :documents, :committee_type, :string
   end
--- db/migrate/20161115193010_add_enabled_to_settings.digestifier.rb
+++ db/migrate/20161115193010_add_enabled_to_settings.digestifier.rb
@@ -1,5 +1,5 @@
 # This migration comes from digestifier (originally 3)
-class AddEnabledToSettings < ActiveRecord::Migration
+class AddEnabledToSettings < ActiveRecord::Migration[5.0]
   def change
     add_column :digestifier_settings, :enabled, :boolean, default: true,
       null: false

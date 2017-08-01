61e47dc1b8eab821dc2a28444c2e6e48d9f82e98
Shawn Liu <shawnpliu@gmail.com>
2017-08-01 03:15:21 +0000

    another attempt to get javascript to work on heroku, by adding precompile command to application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.precompile += %w(*.js)
 module Communitygrows
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

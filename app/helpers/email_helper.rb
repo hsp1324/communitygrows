14420a61314a5aa0545ddf6be56581381b9bf5a2
Shawn Liu <shawnpliu@gmail.com>
2017-08-06 13:40:35 +0000

    another typo in email_helper, but different section now

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -120,7 +120,7 @@ module EmailHelper
         User.all.each do |user|
             if user.digest_pref == "real_time"
                 if user.committees.include? committee
-                    NotificationMailer.committee_all_update_email(user, old_name, name, description).deliver
+                    NotificationMailer.committee_update_email(user, old_name, name, description).deliver
                 end
             end
         end

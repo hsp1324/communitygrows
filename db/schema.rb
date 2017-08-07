44c4fc8b45aaeb53dd98165ab114b82501c635d4
wjpark1 <willpark16@gmail.com>
2017-08-06 17:40:41 -0700

    fix:  fixed user profiles to correctly display user names

--- db/schema.rb
+++ db/schema.rb
@@ -161,7 +161,7 @@ ActiveRecord::Schema.define(version: 20170805142414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
-    t.datetime "last_sign_in_at", default: "2017-08-06 16:50:17", null: false
+    t.datetime "last_sign_in_at", default: "2017-08-06 17:34:56", null: false
     t.string "name"
     t.string "board_role"
     t.string "committee"
